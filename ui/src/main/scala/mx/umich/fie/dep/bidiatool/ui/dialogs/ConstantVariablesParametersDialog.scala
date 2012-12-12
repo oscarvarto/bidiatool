d466dc5495ce6067bef7b3a3c5e11ef909d0d361
Oscar Vargas Torres <oscarvarto@gmail.com>
2012-12-12 15:21:29 -0600

    Learning git basics, hehe

---
+++ ui/src/main/scala/mx/umich/fie/dep/bidiatool/ui/dialogs/ConstantVariablesParametersDialog.scala
@@ -0,0 +1,9 @@
+package mx.umich.fie.dep.bidiatool.ui.dialogs
+import swing.{ Label, Frame }
+
+class ConstantVariablesParametersDialog extends Frame {
+  val instructions = new Label("Please assign a constant if desired")
+
+  //val table = new Table(rowData, )
+
+}
